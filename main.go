bbfef2cbcded2aa04ba76d53839b5c027b208e01
codeyu <codeyu@gmail.com>
2017-04-09 23:22:48 +0800

    feature: ok

--- main.go
+++ main.go
@@ -4,18 +4,29 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 	"os"
 	"path"
+	"path/filepath"
+	"strings"
 )
 
-type FileInfo struct {
+const FILE_PATH = "./duokan/"
+
+type FileDesc struct {
 	Id   int    `json:"id"`
 	Size int64  `json:"size"`
 	Name string `json:"name"`
 	Path string `json:"path"`
 }
 
+func init() {
+	err := initFilePath(FILE_PATH)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
 func main() {
 	mux := http.NewServeMux()
 	fs := http.FileServer(http.Dir("www"))
@@ -51,18 +62,43 @@ func handleRequest(w http.ResponseWriter, r *http.Request) {
 func handleGet(w http.ResponseWriter, r *http.Request) (err error) {
 	fmt.Println("GET: " + r.URL.Path)
 	name := path.Base(r.URL.Path)
-	fmt.Println("download: " + name)
-	file, err := retrieve()
-	if err != nil {
+	if strings.Contains(name, ".") {
+		fmt.Println("download: " + name)
+		file := FILE_PATH + name
+		if exist, _ := exists(file); !exist {
+			http.NotFound(w, r)
+		}
+		http.ServeFile(w, r, file)
+		return
+	} else {
+		_dirpath, err1 := os.Open(FILE_PATH)
+		if err1 != nil {
+			err = err1
+			return
+		}
+		_dir, err1 := _dirpath.Readdir(0)
+		if err1 != nil {
+			err = err1
+			return
+		}
+		files := make([]FileDesc, len(_dir))
+		for i, f := range _dir {
+			files[i].Id = i + 1
+			files[i].Name = f.Name()
+			files[i].Path = ""
+			files[i].Size = f.Size()
+
+		}
+		output, err1 := json.MarshalIndent(&files, "", "\t\t")
+		if err1 != nil {
+			err = err1
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(output)
 		return
 	}
-	output, err := json.MarshalIndent(&file, "", "\t\t")
-	if err != nil {
-		return
-	}
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(output)
-	return
+
 }
 
 func handlePost(w http.ResponseWriter, r *http.Request) (err error) {
@@ -75,7 +111,7 @@ func handlePost(w http.ResponseWriter, r *http.Request) (err error) {
 	}
 	defer file.Close()
 	fmt.Fprintf(w, "%v", handler.Header)
-	f, err := os.OpenFile("./"+handler.Filename, os.O_WRONLY|os.O_CREATE, 0666)
+	f, err := os.OpenFile(FILE_PATH+handler.Filename, os.O_WRONLY|os.O_CREATE, 0666)
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -88,17 +124,47 @@ func handleDelete(w http.ResponseWriter, r *http.Request) (err error) {
 	fmt.Println("DELETE: " + r.URL.Path)
 	name := path.Base(r.URL.Path)
 	fmt.Println("DELETE: " + name)
+	err = os.Remove(FILE_PATH + name)
 	if err != nil {
-		return
-	}
-	fileInfo, err := find(name)
-	if err != nil {
-		return
-	}
-	err = fileInfo.delete()
-	if err != nil {
-		return
+		fmt.Println(err)
 	}
 	w.WriteHeader(200)
 	return
+}
+func getCurrentDirectory() string {
+	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	if err != nil {
+		log.Fatal(err)
+	}
+	return strings.Replace(dir, "\\", "/", -1)
+}
+func initFilePath(filePath string) (err error) {
+	if _, err = exists(filePath); err != nil {
+		return
+	}
+	err = os.Mkdir(filePath, 0777)
+	return
+}
+func exists(path string) (bool, error) {
+
+	_, err := os.Stat(path)
+
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+
+	return true, err
+}
+func check(name string) bool {
+	ext := []string{".gif", ".pdf", ".png", ".jpg", ".epub", ".txt"}
+
+	for _, v := range ext {
+		if v == name {
+			return false
+		}
+	}
+	return true
 }
